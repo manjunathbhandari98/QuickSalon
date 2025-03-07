3cd072de13d7511b5d8b95c2c2fe8590af8e1407
Manjunath Bhandari <manjunathbhandari93@gmail.com>
2025-03-07 11:29:22 +0530

    Project Initialized

---
+++ src/main/java/com/quodex/quicksalon/QuicksalonApplication.java
@@ -0,0 +1,13 @@
+package com.quodex.quicksalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicksalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicksalonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quodex/quicksalon/QuicksalonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quodex.quicksalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicksalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
